108436cbc8560b0650f0f1d506c6b841115e9e2c
Ilnova <83817034+Ilnova@users.noreply.github.com>
2024-11-06 16:00:09 +0100

    Prova test

--- test/test_parking_garage.py
+++ test/test_parking_garage.py
@@ -8,9 +8,14 @@ from src.parking_garage import ParkingGarage
 from src.parking_garage import ParkingGarageError
 
 class TestParkingGarage(TestCase):
-
+#Test sul sensore di parking garage
     @patch.object(GPIO, "input")
-    def test_something(self, mock_object: Mock):
+    def test_check_occupancy(self, mock_distance_sensor: Mock):
         # This is an example of test where I want to mock the GPIO.input() function
-        pass
+        mock_distance_sensor.return_value=True
+        system= ParkingGarage()
+        occupied= system.check_occupancy()
+        self.assertTrue(occupied)
+        #Testing the case where is occupied
+        #Now we have to control the indirect input
 
